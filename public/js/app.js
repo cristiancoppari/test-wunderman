d469af118f6246c917d2578e33a999e33289a805
Cristian Coppari <cristian.coppari@gmail.com>
2021-06-15 22:00:00 -0300

    uploading navbar, hero and section info

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,2 @@
-window.addEventListener("load", () => {
-  console.log("hello world");
-});
+// Global Imports
+import "./utils/navbar.js";
---
+++ public/js/utils/navbar.js
@@ -0,0 +1,37 @@
+const toggleBtn = document.querySelector(".btn-toggle");
+const navList = document.querySelector(".nav-list");
+const navItem = document.querySelector(".nav-item");
+const navItems = [...document.querySelectorAll(".nav-item")];
+
+const showLinksHandler = () => {
+  // Dynamic Approach
+  const navListHeight = navList.getBoundingClientRect().height;
+  const elementStyle = window.getComputedStyle(navItem);
+  const elementHeight = elementStyle.getPropertyValue("height").slice(0, 2);
+  const navItemsHeight = elementHeight * navItems.length;
+
+  if (navListHeight === 0) {
+    navList.style.height = `${navItemsHeight}px`;
+  } else {
+    navList.style.height = 0;
+  }
+};
+
+const hideMenuHandler = () => {
+  if (window.innerWidth > 768) {
+    navList.style.height = "auto";
+  } else {
+    navList.style.height = 0;
+  }
+};
+
+navItems.map((item) => {
+  // Closing the links when clicking in a nav item
+  // item.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   navList.style.height = 0;
+  // });
+});
+
+toggleBtn.addEventListener("click", showLinksHandler);
+window.addEventListener("resize", hideMenuHandler);
