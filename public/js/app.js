9b8798494dafb4a5b6b35b645c2280ef5e687a43
Cristian Coppari <cristian.coppari@gmail.com>
2021-06-21 03:45:09 -0300

    adding slick carousel to the cards

--- public/js/app.js
+++ public/js/app.js
@@ -14,8 +14,33 @@ $(document).ready(() => {
   // Slick Carousel
   $('.slick-carousel-cards').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: false,
+    initialSlide: 1,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
 
   // Init Function
