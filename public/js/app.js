d39705360b222000d14ac62be50b06a6bdc4a889
Cristian Coppari <cristian.coppari@gmail.com>
2021-06-21 03:50:06 -0300

    removing the comments in the card carousel script

--- public/js/app.js
+++ public/js/app.js
@@ -37,9 +37,6 @@ $(document).ready(() => {
           infinite: true,
         },
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   });
 
